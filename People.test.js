bbc3c5eb38e3c6f9212e2e8bb36967d4984f8e59
abdulahi roble <abdulahi_2610@hotmail.com>
2022-10-05 13:38:25 +0200

    Read from json

--- People.test.js
+++
@@ -1,7 +0,0 @@
-const People = require("./People")
-
-test('Return a fake full name and gender', () => {
-    const people = new People()
-    expect(people.fullNameAndGender("abdulahi mohamed male")).toBe("abdulahi mohamed male")
-})
-
