849f06dd30972799263304a29b05135e0291a648
Andreas Pedersen <31003640+AndreasP-92@users.noreply.github.com>
2022-10-08 17:36:20 +0200

    update

--- test/People.test.js
+++ test/People.test.js
@@ -1,19 +1,71 @@
-const People = require("../src/People")
+const People = require("../src/People");
+// const services = require("../src/services");
 const list = require("../src/main")
 
-test('Return a fake cpr', () => {
-    const people = new People()
-    const peopleCpr = list.map((test) => test.cpr)
-    people.cpr = list.map((test) => test.cpr)
-    expect(people.cprNumber(people.cpr)).toStrictEqual(peopleCpr)
+const frank = new People("Frank", "Lorenzen", "male", "1234578911", "19/04/1996");
+const jasmine = new People("jasmine", "Lorenzen", "male", "12345789100000", "19/04/1996");
+
+let cases = [[frank, true], [jasmine, false]]
+
+describe('Validate CPR Length', () => {
+    test.each(cases)(
+        "given %p and %p as arguments, returns %p",
+        (obj, expectedResult) => {
+            console.log(obj, expectedResult)
+            const result = obj.getCpr().length == 10;
+            expect(result).toEqual(expectedResult);
+        }
+
+    )
 })
 
-test('Return a full name and gender', () => {
-    const people = new People()
-    // const peopleName = list.map((test) => people.name == test.name)
-    const peopleName = list.map((test) => console.log(test.name))
+describe('Validate CPR Length', () => {
+    test.each(cases)(
+        "given %p and %p as arguments, returns %p",
+        (obj, expectedResult) => {
+            console.log(obj, expectedResult)
+            const result = obj.getCpr().length == 0;
+            expect(result).toEqual(expectedResult);
+        }
 
-    expect(people.fullNameAndGender(people.name)).toBe(peopleName)
+    )
 })
 
+describe('Validate CPR Length', () => {
+    test.each(cases)(
+        "given %p and %p as arguments, returns %p",
+        (obj, expectedResult) => {
+            console.log(obj, expectedResult)
+            const result = obj.getCpr().length == 11;
+            expect(result).toEqual(expectedResult);
+        }
+
+    )
+})
+
+describe('Validate CPR Length', () => {
+    test.each(cases)(
+        "given %p and %p as arguments, returns %p",
+        (obj, expectedResult) => {
+            console.log(obj, expectedResult)
+            const result = obj.getCpr().length == 11;
+            expect(result).toEqual(expectedResult);
+        }
+
+    )
+})
+
+// test("Testing the class", ()=>{
+//     const people = new People("name her", "surname her", "gender her", "cpr her", "birthday her");
+//     // people.name = "HEJ"
+//     console.log(people.getName())
+// })
+
+// test('Return a full name and gender', () => {
+//     const people = new People()
+//     // const peopleName = list.map((test) => people.name == test.name)
+//     const peopleName = list.map((test) => console.log(test.name))
+
+//     expect(people.fullNameAndGender(people.name)).toBe(peopleName)
+// })
 
